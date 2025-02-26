0f8085935528eed55bd309845db2df62ba001084
perchig <ivanslepko11@gmail.com>
2025-02-26 11:02:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-
+  if (a % b == 0)
+    return b;
+  if (b  % a == 0)
+    return a;
+  if (a > b)
+    return gcd(a % b, b);
+  else
+    return gcd(a, b % a);
 }
